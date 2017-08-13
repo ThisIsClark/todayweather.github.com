19e975708530367c315958938da8077ca95a34af
todayweather <liuyu@liuyudeAir.lan>
2017-08-13 14:32:19 +0800

    release webAR

--- AR_Project/config/text_config.js
+++
@@ -1,7 +0,0 @@
-var home_info = "你无法叫醒一个不回你消息的人，<br/>但是红包能。<br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;——王尚·查得";
-var home_tip1 = "为让您获取最佳体验，已将屏幕亮度调整到最高。";
-var home_tip2 = "本应用不支持在电脑上使用，请用手机扫描二维码后打开此应用。";
-var home_tip3 = "本程序需要获取您的GPS定位后才能运行。";
-var home_tip4 = "本程序需要获取您的摄像头权限后才能运行。"
-var loading1_title = "请您稍后";
-var loading1_content = "富强、民主、文明<br/>和谐、自由、平等<br/>公正、法治、爱国<br/>敬业、诚信、友善";
